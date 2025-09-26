b03008721140a5b3430ebdce69f61690240976b2
Kayky Dias <kayky.oliveira@perpart.pe.gov.br>
2025-09-26 11:34:46 -0300

    Ajustes visuais na homepage

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -16,11 +16,11 @@ const Homepage = () => {
 
   return (
     <div className="bg-stone-100 min-h-screen flex items-center justify-center p-4">
-      <div className="relative bg-white rounded-2xl shadow-xl/20 p-16 md:p-24 w-400 h-200 overflow-hidden">
+      <div className="relative bg-white rounded-2xl shadow-xl w-full max-w-6xl p-12 md:p-12 overflow-hidden flex flex-col gap-12 md:gap-16 min-h-[600px]">
         <div className="absolute top-0 left-0 w-full h-full pointer-events-none">
           <svg
             className="absolute top-0 left-0 w-full h-auto"
-            viewBox="0 0 1000 300"
+            viewBox="0 0 950 300"
             preserveAspectRatio="none"
           >
             <path
@@ -30,29 +30,29 @@ const Homepage = () => {
           </svg>
         </div>
 
-        <div className="relative z-10 flex justify-between   items-start mb-32">
-          <h1 className="text-blue-500 font-bold text-8xl md:text-[9rem]">
+        <div className="relative z-10 w-full flex items-center justify-between">
+          <h1 className="text-blue-900 font-bold text-5xl sm:text-6xl md:text-7xl">
             S.O.S Escola
           </h1>
-          <div className="absolute -top-16 -right-2 w-56 h-56 md:w-72 md:h-72 bg-sky-500 rounded-full flex items-center justify-center shadow-md">
-            <img src={Logo} alt="Logo" className="w-40 h-40 md:w-48 md:h-48" />
+          <div className="relative w-24 h-24 md:w-40 md:h-40 bg-sky-700 rounded-full flex items-center justify-center shadow-md mr-28">
+            <img src={Logo} alt="Logo" className="w-16 h-16 md:w-25 md:h-25" />
           </div>
         </div>
 
-        <div className="fixed mt-20 justify-center items-center z-10">
-          <h2 className="text-5xl font-bold text-gray-800">
+        <div className="relative z-10 flex flex-col items-center md:items-start gap-8">
+          <h2 className="text-3xl md:text-4xl font-bold text-gray-800 text-center md:text-left md:mt-4 ">
             Denúncias Anônimas de Bullying
           </h2>
 
-          <div className="flex flex-col gap-10 items-start mt-30 ">
+          <div className="flex flex-col gap-6 mt-6 justify-center md:justify-start">
             <Button
-              className="bg-stone-200 w-150 text-stone-800 text-3xl font-semibold cursor-pointer py-6 px-16 rounded-full transition-colors hover:bg-stone-300"
+              className="bg-stone-200 text-stone-800 text-xl w-128 h-10 md:text-2xl font-semibold cursor-pointer py-4 px-8 rounded-full transition-colors hover:bg-stone-300"
               onClick={handleTeacherClick}
             >
               Área do Professor
             </Button>
             <Button
-              className="bg-stone-200 w-150 text-stone-800 text-3xl font-semibold cursor-pointer py-6 px-16 rounded-full transition-colors hover:bg-stone-300"
+              className="bg-stone-200 text-stone-800 text-xl w-128 h-10 md:text-2xl font-semibold cursor-pointer py-4 px-8 rounded-full transition-colors hover:bg-stone-300"
               onClick={handleStudentClick}
             >
               Área do Aluno
