57655ad3d0d2c3ede6844b85540ec98a3596598c
Kayky <kaykydiasdi@gmail.com>
2025-09-26 23:12:04 -0300

    Ajuste nos imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './pages/ADM/login';
+import Login from './pages/login';
 import {MainLayout} from './pages/ADM/dashboard'; // <- sem { } se for export default
 import { DenunciaRec } from './pages/ADM/DenunciaRec';
 import { Relatorio } from './pages/ADM/relatorio';
